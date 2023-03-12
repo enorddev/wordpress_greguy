97fb4d88d3227db74bef7ed924079ebd7a284c47
Edwin Nord <edwin.nord09@gmail.com>
2023-03-11 22:23:33 -0500

    first commit

---
+++ wordpress/wp-content/themes/greguy_theme/functions.php
@@ -0,0 +1,68 @@
+<?php
+
+function adv_theme_support() {
+    register_nav_menus(array(
+        'primary'  => __('Primary Menu'),
+        'footer'   => __('Footer Menu')
+    ));
+}
+
+add_action('after_setup_theme', 'adv_theme_support' );
+
+ 
+function init_widget($id) {
+    register_sidebar(array(
+        'name' => 'Box 1',
+        'id'   => 'box1',
+        'before_widget' => '<div class="box box1">',
+        'after_widget'  => '</div>',
+        'before_title' => '<h3>',
+        'after_title'  => '</h3>'
+    ));
+
+    register_sidebar(array(
+        'name' => 'Box 2',
+        'id'   => 'box2',
+        'before_widget' => '<div class="box box2">',
+        'after_widget'  => '</div>',
+        'before_title' => '<h3>',
+        'after_title'  => '</h3>'
+    ));
+
+    register_sidebar(array(
+        'name' => 'Box 3',
+        'id'   => 'box3',
+        'before_widget' => '<div class="box box3">',
+        'after_widget'  => '</div>',
+        'before_title' => '<h3>',
+        'after_title'  => '</h3>'
+    ));
+
+    register_sidebar(array(
+        'name' => 'Desc 1',
+        'id'   => 'desc1',
+        'before_widget' => '<div class="desc1">',
+        'after_widget'  => '</div>',
+        'before_title' => '<h2>',
+        'after_title'  => '</h2>'
+    ));
+
+    register_sidebar(array(
+        'name' => 'Desc 2',
+        'id'   => 'desc2',
+        'before_widget' => '<div class="desc2">',
+        'after_widget'  => '</div>',
+        'before_title' => '<h2>',
+        'after_title'  => '</h2>'
+    ));
+
+    register_sidebar(array(
+        'name' => 'Desc 3',
+        'id'   => 'desc3',
+        'before_widget' => '<div class="desc3">',
+        'after_widget'  => '</div>',
+        'before_title' => '<h2>',
+        'after_title'  => '</h2>'
+    ));
+}
+add_action('widgets_init', 'init_widget');
---
+++ wordpress/wp-content/themes/greguy_theme/header.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html <?php language_attributes() ?>>
+
+<head>
+    <meta <?php bloginfo('charset') ?> />
+    <meta name="viewport" content="<?php bloginfo('description') ?>" />
+    <title>
+        <?php bloginfo('name') ?>
+        <?php is_front_page() ? bloginfo('description') : wp_title() ?>
+    </title>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?> /css/mdb.min.css" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?> /style.css ">
+    <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
+    <script defer src="<?php bloginfo('template_url'); ?> /js/app.js"></script>
+    <?php wp_head() ?>
+</head>
+
+<body <?php body_class() ?>>
+    <div class="container-fluid">
+
+        <header>
+
+            <nav class="navbar navbar-expand-xxl navbar-light bg-light">
+                <div class="container-fluid nav-container">
+                    <a class="navbar-brand" href="<?php echo site_url() ?>">
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?> /img/logo-black.png" alt="kw_logo">
+                    </a>
+
+                    <button onclick="openNav()" class="navbar-toggler" type="button" data-mdb-target="#navbarExample01" aria-controls="navbarExample01" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-bars"></i>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarExample01">
+
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+
+                            <?php
+                            $args = array(
+                                'theme_location' => 'primary'
+                            );
+
+                            ?>
+
+                            <?php wp_nav_menu($args); ?>
+                        </ul>
+
+                    </div>
+
+
+                    <div id="mySidenav" class="sidenav">
+
+
+
+                        <div class="menu active">
+                            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                        </div>
+
+
+                        <div class="menu-links active">
+
+
+                            <div class="nav-links">
+                                <?php
+                                $args = array(
+                                    'theme_location' => 'primary'
+                                );
+
+                                ?>
+
+                                <?php wp_nav_menu($args); ?>
+                            </div>
+                        </div>
+
+
+
+                    </div>
+
+                </div>
+
+            </nav>
